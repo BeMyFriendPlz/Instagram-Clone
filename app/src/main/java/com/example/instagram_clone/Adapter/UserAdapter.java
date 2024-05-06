4c7b1ca5f2914a868cb49a8767a4e815debb0092
BeMyFriendPlz <xuanhieunguyen25902@gmail.com>
2024-05-06 11:39:22 +0700

    Update SearchFragment + UserAdapter

---
+++ app/src/main/java/com/example/instagram_clone/Adapter/UserAdapter.java
@@ -0,0 +1,106 @@
+package com.example.instagram_clone.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.instagram_clone.Model.User;
+import com.example.instagram_clone.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private Context mContext;
+    private List<User> mUsers;
+    private boolean isFragment;
+
+    private FirebaseUser firebaseUser;
+
+    public UserAdapter(Context mContext, List<User> mUsers, boolean isFragment) {
+        this.mContext = mContext;
+        this.mUsers = mUsers;
+        this.isFragment = isFragment;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        User user = mUsers.get(position);
+        holder.btnFollow.setVisibility(View.VISIBLE);
+
+        holder.username.setText(user.getUsername());
+        holder.fullname.setText(user.getName());
+
+        Picasso.get().load(user.getImageurl()).placeholder(R.mipmap.ic_launcher).into(holder.imageProfile);
+
+        isFollowed(user.getId(), holder.btnFollow);
+
+        if (user.getId().equals(firebaseUser.getUid())) {
+            holder.btnFollow.setVisibility(View.GONE);
+        }
+    }
+
+    private void isFollowed(String id, Button btnFollow) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Follow")
+                .child(firebaseUser.getUid()).child("following");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(id).exists()) {
+                    btnFollow.setText("Following");
+                } else {
+                    btnFollow.setText("Follow");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public CircleImageView imageProfile;
+        public TextView username, fullname;
+        public Button btnFollow;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageProfile = itemView.findViewById(R.id.image_profile);
+            username = itemView.findViewById(R.id.username);
+            fullname = itemView.findViewById(R.id.fullname);
+            btnFollow = itemView.findViewById(R.id.btn_follow);
+        }
+    }
+}
--- app/src/main/java/com/example/instagram_clone/Fragments/SearchFragment.java
+++ app/src/main/java/com/example/instagram_clone/Fragments/SearchFragment.java
@@ -2,19 +2,76 @@ package com.example.instagram_clone.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.example.instagram_clone.Adapter.UserAdapter;
+import com.example.instagram_clone.Model.User;
 import com.example.instagram_clone.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.hendraanggrian.appcompat.socialview.widget.SocialAutoCompleteTextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private List<User> mUsers;
+    private UserAdapter userAdapter;
+    private SocialAutoCompleteTextView search_bar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        recyclerView = view.findViewById(R.id.recycle_view_users);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mUsers = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext(), mUsers, true);
+        recyclerView.setAdapter(userAdapter);
+
+        search_bar = view.findViewById(R.id.search_bar);
+
+        readUsers();
+
+        return view;
+    }
+
+    private void readUsers() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (TextUtils.isEmpty(search_bar.getText().toString())) {
+                    mUsers.clear();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        User user = dataSnapshot.getValue(User.class);
+                        mUsers.add(user);
+                    }
+
+                    userAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
